4a65a4e56d211705bd25fa30f3cfaaf966b9f120
Marina Ma <marina@ridecell.com>
2020-05-13 21:04:35 -0700

    trigger test_rds step by modifying an rds related file

--- pkg/controller/rds/components/rds_instance.go
+++ pkg/controller/rds/components/rds_instance.go
@@ -39,6 +39,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
+// throwing in a comment here to trigger test_rds run.
 const RDSInstanceDatabaseFinalizer = "rdsinstance.database.finalizer"
 
 type rdsInstanceComponent struct {
